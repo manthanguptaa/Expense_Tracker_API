0458a2b2ab5436029ebbf246e52cff9f872d624f
Manthan Gupta <manthangupta109@gmail.com>
2021-05-26 12:55:01 +0530

    CORS

--- src/main/java/com/manthan/expensetracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/manthan/expensetracker/ExpenseTrackerApiApplication.java
@@ -2,18 +2,33 @@ package com.manthan.expensetracker;
 
 import com.manthan.expensetracker.Filters.AuthFilter;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import ch.qos.logback.core.filter.Filter;
 
 @SpringBootApplication
 public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);}
+
+		@Bean
+		public FilterRegistrationBean<CorsFilter> corsFilter(){
+			FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			CorsConfiguration config = new CorsConfiguration();
+			config.addAllowedOrigin("*");
+			config.addAllowedHeader("*");
+			source.registerCorsConfiguration("/**",config);
+			registrationBean.setFilter(new CorsFilter());
+			registrationBean.setOrder(0);
+			return registrationBean;
+		}
 	
 		@Bean
 		public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
